f90ecfd5619b4502d02cf38aebbc1ed8d1bd2a40
breini43 <breini43@github.com>
2023-01-15 01:25:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+ * Это действие позволяет пользователю бросить кубик и просмотреть результат
+ * на экране.
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /**
+     * Бросьте кости и обновите экран результатом.
+     */
+
+    private fun rollDice() {
+
+        // Создайте новый объект из кубиков с 6 сторонами и бросьте его
+        val kubik = Kubik(6)
+        val kubikRoll = kubik.roll()
+
+        // Обновите экран с помощью броска кубиков
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = kubikRoll.toString()
+    }
+}
+
+class Kubik(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
