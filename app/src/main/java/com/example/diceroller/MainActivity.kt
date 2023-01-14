7da8542c1fff900a8bc150663fb10a8486815b70
breini43 <breini43@github.com>
2023-01-15 01:59:10 +0300

    add kubik №2

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -29,15 +29,25 @@ class MainActivity : AppCompatActivity() {
         // Создайте новый объект из кубиков с 6 сторонами и бросьте его
         val kubik = Kubik(6)
         val kubikRoll = kubik.roll()
+        val kubik2 = Kubik2(6)
+        val kubikRoll2 = kubik2.roll()
 
         // Обновите экран с помощью броска кубиков
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = kubikRoll.toString()
+        val resultTextViews: TextView = findViewById(R.id.textView2)
+        resultTextViews.text = kubikRoll2.toString()
     }
 }
 
 class Kubik(private val numSides: Int) {
     fun roll(): Int {
         return (1..numSides).random()
+    }
+}
+
+class Kubik2(val numSidess: Int) {
+    fun roll():Int{
+        return (1..numSidess).random()
     }
 }
